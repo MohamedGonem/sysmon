4ce757d610910ae2aeea2bba9999a2f3badfc73f
Mohamed Gonem <nightangelyt01@gmail.com>
2026-05-01 18:38:17 +0300

    add cpu module and usage stat struct

---
+++ src/cpu.rs
@@ -0,0 +1,13 @@
+
+struct UsageStat {
+    user: u64,
+    nice: u64,
+    system: u64,
+    idle: u64,
+    iowait: u64,
+    irq: u64,
+    softirq: u64,
+    steal: u64,
+    guest: u64,
+    guest_nice: u64,
+}
