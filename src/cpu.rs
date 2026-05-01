7396a951785606a3ee469f59848961987d8e6038
Mohamed Gonem <nightangelyt01@gmail.com>
2026-05-01 18:57:34 +0300

    add function to color usage stat struct and return Vec<String>

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,4 @@
+use crate::utils::{color, color_based_on_percentage, color_head};
 use std::f64;
 
 struct CoreStat {
@@ -18,6 +19,25 @@ struct UsageStat {
     guest: u64,
     guest_nice: u64,
 }
+
+fn color_usagestat(target: UsageStat, tmux: bool) -> Vec<String> {
+    let formatter = |target: u64, text: &str| format!("\t{} {}mb", color_head(text, tmux), target);
+    let user = formatter(target.user, "User");
+    let nice = formatter(target.nice, "Nice");
+    let system = formatter(target.system, "System");
+    let idle = formatter(target.idle, "Idle");
+    let iowait = formatter(target.iowait, "IOWait");
+    let irq = formatter(target.irq, "Irq");
+    let softirq = formatter(target.softirq, "SoftIrq");
+    let steal = formatter(target.steal, "Steal");
+    let guest = formatter(target.guest, "Guest");
+    let guest_nice = formatter(target.guest_nice, "Guest-Nice");
+
+    vec![
+        user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice,
+    ]
+}
+
 struct CPUStat {
     model_name: String,
     physical_cores_count: u64,
