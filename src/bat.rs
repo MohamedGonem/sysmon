be77c2f2aa1cd988e4bb867bd86e222a42285175
MohamedGonem <nightangelyt01@gmail.com>
2026-05-14 18:48:02 +0300

    fix: no bat crash

--- src/bat.rs
+++ src/bat.rs
@@ -6,25 +6,31 @@ struct BatteryStat {
     short_status: String,
 }
 
-fn battery_status() -> BatteryStat {
+fn battery_status() -> Option<BatteryStat> {
     let capacity: u64 = std::fs::read_to_string("/sys/class/power_supply/BAT0/capacity")
-        .unwrap()
+        .ok()?
         .trim()
         .parse()
-        .unwrap();
+        .ok()?;
+
     let full_status = std::fs::read_to_string("/sys/class/power_supply/BAT0/status")
         .unwrap_or_default()
         .trim()
         .to_string();
+
     let shortened_status = full_status.get(..3).unwrap_or(&full_status).to_string();
-    BatteryStat {
+
+    Some(BatteryStat {
         percentage: capacity,
         status: full_status,
         short_status: shortened_status,
-    }
+    })
 }
 pub fn print_battery(arg_env: String) {
-    let bat = battery_status();
+    let Some(bat) = battery_status() else {
+        println!("No battery");
+        return;
+    };
     let tmux = arg_env == "tmux";
     let bat = color_bat(bat.percentage, tmux, bat.status, bat.short_status);
     println!("{} {} {}", bat[0], bat[1], bat[2]);
