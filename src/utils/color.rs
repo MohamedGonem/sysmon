b909af08c95a07359078c5308f5c87408898df25
MohamedGonem <nightangelyt01@gmail.com>
2026-05-27 16:36:58 +0300

    add tmux mod for tmux only coloring

--- src/utils/color.rs
+++ src/utils/color.rs
@@ -28,3 +28,37 @@ pub fn bright_green_bold(text: &str) -> String {
 pub fn bright_red(text: &str) -> String {
     format!("\x1b[91m{}\x1b[0m", text)
 }
+
+#[cfg(feature = "tmux")]
+pub mod tmux {
+    pub fn green(text: &str) -> String {
+        format!("#[fg=green]{}", text)
+    }
+    pub fn green_bold(text: &str) -> String {
+        format!("#[fg=brightgreen]{}", text)
+    }
+    pub fn yellow(text: &str) -> String {
+        format!("#[fg=yellow]{}", text)
+    }
+    pub fn yellow_bold(text: &str) -> String {
+        format!("#[fg=brightyellow]{}", text)
+    }
+    pub fn red(text: &str) -> String {
+        format!("#[fg=red]{}", text)
+    }
+    pub fn red_bold(text: &str) -> String {
+        format!("#[fg=brightred]{}", text)
+    }
+    pub fn white_bold(text: &str) -> String {
+        format!("#[fg=brightwhite]{}", text)
+    }
+    pub fn bright_blue_bold(text: &str) -> String {
+        format!("#[fg=brightblue]{}", text)
+    }
+    pub fn bright_green_bold(text: &str) -> String {
+        format!("#[fg=brightgreen]{}", text)
+    }
+    pub fn bright_red(text: &str) -> String {
+        format!("#[fg=brightred]{}", text)
+    }
+}
